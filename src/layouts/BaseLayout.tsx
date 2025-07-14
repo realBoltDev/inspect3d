1cecba9502a48d62b07c8e34d424300b75822573
beastxbolt <harsit2405@gmail.com>
2025-07-14 22:39:09 +0530

    feat: Initial commit

---
+++ src/layouts/BaseLayout.tsx
@@ -0,0 +1,15 @@
+import { Outlet } from 'react-router-dom';
+import { Header } from '@/components/Header';
+import { Footer } from '@/components/Footer';
+
+export function BaseLayout() {
+  return (
+    <>
+      <Header />
+      <main>
+        <Outlet />
+      </main>
+      <Footer />
+    </>
+  );
+}
