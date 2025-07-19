545cf627526ed324bc6144a0e11e49884945351f
beastxbolt <harsit2405@gmail.com>
2025-07-19 10:07:01 +0530

    feat: Add base layout component

--- src/layouts/BaseLayout.tsx
+++ src/layouts/BaseLayout.tsx
@@ -1,15 +1,29 @@
+import { AppShell } from '@mantine/core';
+import { useDisclosure } from '@mantine/hooks';
 import { Outlet } from 'react-router-dom';
-import { Header } from '@/components/Header';
-import { Footer } from '@/components/Footer';
+import { Header } from '@/components/Header/Header';
+import { Sidebar } from '@/components/Sidebar/Sidebar';
 
 export function BaseLayout() {
+  const [opened, { toggle }] = useDisclosure();
+
   return (
-    <>
-      <Header />
-      <main>
+    <AppShell
+      header={{ height: 60 }}
+      navbar={{ width: 300, breakpoint: 'sm', collapsed: { mobile: !opened } }}
+      style={{ height: '100vh' }}
+    >
+      <AppShell.Header>
+        <Header opened={opened} toggle={toggle} />
+      </AppShell.Header>
+
+      <AppShell.Navbar p="md">
+        <Sidebar />
+      </AppShell.Navbar>
+
+      <AppShell.Main style={{ height: '100%', overflow: 'hidden' }}>
         <Outlet />
-      </main>
-      <Footer />
-    </>
+      </AppShell.Main>
+    </AppShell>
   );
 }
